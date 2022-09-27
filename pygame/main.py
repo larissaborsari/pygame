8e02d1cc1d1977bc76c603afa61939044b808558
Lari <larissaborsari@hotmail.com>
2022-09-27 15:06:09 -0300

    commit inicial

---
+++ pygame/main.py
@@ -0,0 +1,7 @@
+import pygame
+
+#initiating pygame
+pygame.init()
+
+#creating the display surface
+screen = pygame.display.set_mode((width, height)) #contains a tuple with width and height of the game window
