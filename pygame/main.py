7a1bd5e464d5e0c2933a92d28fcb5719db15e0c5
Lari <larissaborsari@hotmail.com>
2022-09-28 15:56:45 -0300

    last commit

--- pygame/main.py
+++ pygame/main.py
@@ -8,6 +8,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400)) #contains a tuple with width and height of the game window
 #changing the title of the game in the window
 pygame.display.set_caption('Jump It')
+clock = pygame.time.Clock()
+test_surface = pygame.Surface()
 
 while True: #keep the code running while the game isn't over
     for event in pygame.event.get(): #user interaction
@@ -16,3 +18,5 @@ while True: #keep the code running while the game isn't over
             exit()
 
     pygame.display.update() #update the display surface
+    clock.tick(60) #the game should not run faster than 60 fps
+
