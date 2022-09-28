30c81d114193d9aa05ace3293e41539c6c64c44f
Lari <larissaborsari@hotmail.com>
2022-09-28 09:13:04 -0300

    last commit

--- pygame/main.py
+++ pygame/main.py
@@ -4,4 +4,7 @@ import pygame
 pygame.init()
 
 #creating the display surface
-screen = pygame.display.set_mode((width, height)) #contains a tuple with width and height of the game window
+screen = pygame.display.set_mode((800, 400)) #contains a tuple with width and height of the game window
+
+while True: #keep the code running while the game isn't over
+    pygame.display.update() #update the display surface
